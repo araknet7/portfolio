2b681133012e948fbe45c01eabe6d8152ecd0f9e
araknet7 <86892627+araknet7@users.noreply.github.com>
2021-07-03 15:03:50 -0500

    first commit

---
+++ webform.php
@@ -0,0 +1,28 @@
+<?php
+// echo "<pre>";
+//     print_r($_POST);
+// echo "</pre>";
+    $message_sent = false;
+    if(isset($_POST['email']) && $_POST['email'] != '') {
+        
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            //submit the form
+            $userName = $_POST["name"];
+            $userEmail = $_POST["email"];
+            $messageSubject = $_POST["messageSubject"];
+            $message = $_POST["message"];
+
+            $to = "sonofchristen@fastmail.com";
+            $body = "";
+
+            $body .= "From: ".$userName. "\r\n";
+            $body .= "Email: ".$userEmail. "\r\n";
+            $body .= "Message: ".$message. "\r\n";
+
+            mail($to,$messageSubject,$body);
+
+            $message_sent = true;
+        }
+    }
+
+?>
